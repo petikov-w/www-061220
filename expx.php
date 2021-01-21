cdf5d58d12f3ffdc06d4fbed778a904478791dbb
Petikov Vladimir <vx7xv@inbox.ru>
2021-01-21 11:31:19 +0300

    Проект "Копилка ссылок", получение данных с сервера - продолжение 2

--- expx.php
+++ expx.php
@@ -1,4 +1,17 @@
 <?php
-$west = $_POST['west'];
-$result = $west . ' ответ с сервера!';
-echo json_encode($result);
+//$west = $_POST['juja'];
+//$sss = json_decode($west);
+//printss($_POST);
+//$result = $west . ' ответ с сервера!';
+//echo json_encode($result);
+
+
+header("Content-Type: application/json");
+// разбираем JSON-строку на составляющие встроенной командой
+$data = json_decode(file_get_contents("php://input"));
+// отправляем в ответ строку с подтверждением
+echo json_decode($data);
+//echo "Сервер получил следующие данные: mas — $data->mas, pas — $data->pas";
+
+if ($data->pas == 'Пока') {echo 'Досвидания';}
+    else {echo '---------';}
--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,22 @@
-function ajax(url, method, functionName, dataArray) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.open(method, url, true);
-    xhttp.setRequestHeader('Content-type', 'application/json');
-    xhttp.send(requestData(dataArray));
-
-    xhttp.onreadystatechange = function () {
-        if (this.readyState==4 && this.status==200) {
-            functionName(this);
-        }
-    }
-}
-
-function requestData(dataArr) {
-    let out = '';
-        for (let key in dataArr){
-            out += `${key} = ${dataArr[key]}`;
-    }
-    console.log(out);
-    return out;
-}
+// function ajax(url, method, functionName, dataArray) {
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.open(method, url, true);
+//     xhttp.setRequestHeader('Content-type', 'application/json');
+//     xhttp.send(requestData(dataArray));
+//
+//     xhttp.onreadystatechange = function () {
+//         if (this.readyState==4 && this.status==200) {
+//             functionName(this);
+//         }
+//     }
+// }
+//
+// function requestData(dataArr) {
+//     let out = '';
+//     $_POST['juja'] = JSON.stringify(ss01);
+//     // for (let key in dataArr){
+//     //         out += `${key} = ${dataArr[key]}`;
+//     // }
+//     console.log(out);
+//     return  $_POST['juja'];
+// }
